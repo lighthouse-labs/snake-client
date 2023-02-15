3e28463d3dcdedbb01e98971f280e150cacf1772
cniscoding <asdfa.com>
2023-02-15 14:21:21 -0800

    update input.js to include movement keys

--- input.js
+++ input.js
@@ -12,10 +12,28 @@ const handleUserInput = function() {
   stdin.on('data', (key) =>{
     if (key === '\u0003') {
       // console.log('press exit')
-      process.exit();
+  
+    }
+    if (key === 'w') {
+// "Move: up" - move up one square (unless facing down)
+      conn.write("Move: up");
+    }
+    if (key === 'a') {
+// "Move: left" - move left one square (unless facing right)
+      conn.write("Move: left");
+    }
+    if (key === 's') {
+// "Move: down" - move down one square (unless facing up)
+      conn.write("Move: down");
+    }
+    if (key === 'd') {
+// "Move: right" - move left one square (unless facing left)
+      conn.write("Move: right");
     }
   });
 };
+
+
 
 module.exports = {
   setupInput,
