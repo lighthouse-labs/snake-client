e8c4cb88cac4479b5b04bcde4439df4183b5c9d0
cniscoding <asdfa.com>
2023-02-15 18:58:04 -0800

    working snake

--- client.js
+++ client.js
@@ -1,20 +1,22 @@
 const net = require("net");
+
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: '50541',
+    host: 'localhost',
+    port: 50541,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("data", (message)=>{
     console.log(message);
-
+    
   });
-
+  
   conn.on("connect", ()=>{
     console.log('Successfully connected to game server');
-    conn.write("Name: MGK");
+    conn.write("Name: LXK");
+    // setInterval(()=>{conn.write("Move: up")},500)
   });
 
   return conn;
--- input.js
+++ input.js
@@ -1,6 +1,9 @@
-const stdin = process.stdin;
 
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -8,33 +11,32 @@ const setupInput = function() {
   return stdin;
 };
 
-const handleUserInput = function() {
-  stdin.on('data', (key) =>{
-    if (key === '\u0003') {
-      // console.log('press exit')
-  
-    }
-    if (key === 'w') {
-// "Move: up" - move up one square (unless facing down)
-      conn.write("Move: up");
-    }
-    if (key === 'a') {
-// "Move: left" - move left one square (unless facing right)
-      conn.write("Move: left");
-    }
-    if (key === 's') {
-// "Move: down" - move down one square (unless facing up)
-      conn.write("Move: down");
-    }
-    if (key === 'd') {
-// "Move: right" - move left one square (unless facing left)
-      conn.write("Move: right");
-    }
-  });
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log('press exit');
+    process.exit();
+  }
+  if (key === 'w') {
+    // "Move: up" - move up one square (unless facing down)
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    // // "Move: left" - move left one square (unless facing right)
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    // // "Move: down" - move down one square (unless facing up)
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    // // "Move: right" - move left one square (unless facing left)
+    connection.write("Move: right");
+  }
 };
+
 
 
 
 module.exports = {
   setupInput,
-}
+};
